bb7c51a09913d272ffdf84213924211d41a12a8e
JoaoVictorArbegaus <joao.v.arbegaus@gmail.com>
2024-04-06 18:26:35 -0300

    Documentação

--- public/models/WaterIntakeCalculator.js
+++ public/models/WaterIntakeCalculator.js
@@ -1,5 +1,13 @@
+/**
+ * Classe que calculará a quantidade de agua que o usuário deverá beber.
+ */
 class WaterIntakeCalculator {
-    // quantidade de água necessária com base no peso corporal (35 ml/kg)
+    
+    /**
+    * Calcula a quantidade recomendada de água que o usuário deve tomar com base no peso corporal.
+    * @param {number} weight - O peso corporal do usuário, em quilogramas.
+    * @returns {number} A quantidade recomendada de água a ser consumida diariamente, em mililitros.
+    */
     static calculateWaterIntake(weight) {
         const intakeFactor = 35; // ml/kg
 
--- public/models/database.js
+++ public/models/database.js
@@ -1,11 +1,30 @@
 const Sequelize = require('sequelize');
 
+/**
+ * Conexão com o banco de dados utilizando Sequelize.
+ * @type {Sequelize}
+ * @typedef {Object} connection
+ * @property {string} database - O nome do banco de dados (railway).
+ * @property {string} username - O nome de usuário para acessar o banco de dados (root).
+ * @property {string} password - A senha para acessar o banco de dados (CiDrFbNtBRDVUqWjRVpdEqtOAmCGDjjo).
+ * @property {Object} options - As opções de configuração para a conexão.
+ * @property {string} options.host - O host do banco de dados (roundhouse.proxy.rlwy.net).
+ * @property {string} options.port - A porta de conexão com o banco de dados (40875).
+ * @property {string} options.dialect - O dialeto do banco de dados (mysql).
+ */
 const connection = new Sequelize ("railway", "root", "CiDrFbNtBRDVUqWjRVpdEqtOAmCGDjjo", {
     host: 'roundhouse.proxy.rlwy.net',
     port: '40875',
     dialect: 'mysql'
 });
 
+
+/**
+ * Autentica a conexão com o banco de dados.
+ * @function authenticate
+ * @memberof connection
+ * @returns {Promise} Uma Promise que é resolvida se a autenticação for bem-sucedida e rejeitada se ocorrer um erro.
+ */
 connection.authenticate().then(function(){
     console.log('Conectado com sucesso ao DB');
 }).catch(function() {
